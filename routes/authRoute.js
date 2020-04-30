aad5cd7ba12dfe2c25618079c4b5cbf0b2db00f0
ilonatr <ilona.tran@metropolia.fi>
2020-04-30 13:06:15 +0300

    auth

---
+++ routes/authRoute.js
@@ -0,0 +1,23 @@
+'use strict';
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const upload = multer({dest: './uploads/'});
+const {body, sanitizeBody} = require('express-validator');
+const authController = require('../controllers/authController');
+
+router.post('/login', authController.login);
+router.get('/logout', authController.logout);
+router.post('/register', upload.single('avatar'),
+    [
+        body('name', 'minimum 3 characters').isLength({min: 3}),
+        body('email', 'email is not valid').isEmail(),
+        body('password', 'at least one upper case letter').
+        matches('(?=.*[A-Z]).{8,}'),
+        //sanitizeBody('name').escape(),
+    ],
+    authController.user_create_post,
+    authController.login,
+);
+
+module.exports = router;
